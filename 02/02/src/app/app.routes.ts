b22cc7c9329f476598bc5e0f91ad5abaa2d0fe4a
mohammedelmoghazi <mohammed.elmoghazi@alareebict.com>
2024-11-10 14:50:17 +0200

    02 ng class

--- 02/02/src/app/app.routes.ts
+++ 02/02/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:'ngif',
         component:NgIfComponent
+    },
+    {
+        path:'ngclass',
+        component:NgClassComponent
     }
 ];
---
+++ 02/02/src/app/ng-class/ng-class.component.ts
@@ -0,0 +1,21 @@
+import { NgClass } from '@angular/common';
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-class',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './ng-class.component.html',
+  styleUrl: './ng-class.component.scss'
+})
+export class NgClassComponent {
+
+  productsList=[
+    {id:1,name:'Product 1', description:'Product 1 description', price:10},
+    {id:2,name:'Product 2', description:'Product 2 description', price:20},
+    {id:3,name:'Product 3', description:'Product 3 description', price:30},
+    {id:4,name:'Product 4', description:'Product 4 description', price:40},
+    {id:5,name:'Product 5', description:'Product 5 description', price:50},
+  ];
+}
