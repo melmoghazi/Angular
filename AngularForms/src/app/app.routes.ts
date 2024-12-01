d3c18564721e17329af402c4e615ffbd3a3e44d5
melmoghazi <m.elmoghazi@gmail.com>
2024-12-01 10:12:52 +0200

    AngularForms

---
+++ AngularForms/src/app/app.routes.ts
@@ -0,0 +1,50 @@
+import { Routes } from '@angular/router';
+
+import { ProjectStructureComponent } from './shared/components/project-structure/project-structure.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { LoginComponent } from './features/login/login.component';
+import { Login2Component } from './features/login2/login2.component';
+import { RegisterComponent } from './features/reactiveform/register/register.component';
+import { Register2Component } from './features/reactiveform/register2/register2.component';
+import { Register3Component } from './features/reactiveform/register3/register3.component';
+import { Register4Component } from './features/reactiveform/register4/register4.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'project-structure'
+    },
+    {
+        path: 'project-structure',
+        component: ProjectStructureComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'login2',
+        component: Login2Component
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'register2',
+        component: Register2Component
+    },
+    {
+        path: 'register3',
+        component: Register3Component
+    },
+    {
+        path: 'register4',
+        component: Register4Component
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
---
+++ AngularForms/src/app/features/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { SharedModule } from '../../shared/shared.module';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  login(loginForm: NgForm) {
+    console.log('form status', loginForm.status, loginForm.value);
+  }
+
+  login2(loginForm2: NgForm) {
+    console.log('form status', loginForm2.status, loginForm2.value);
+  }
+  email3: string = '';
+  pwd3: string = '';
+
+//object on ther fly
+  loginDto: {
+    email4: string, pwd4: string
+  } = { email4: '', pwd4: '' };
+
+  //object on ther fly
+loginDto5: {
+  email5: string, pwd5: string
+} = { email5: '', pwd5: '' };
+}
